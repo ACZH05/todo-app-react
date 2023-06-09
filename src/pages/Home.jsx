283c27e6adf8a05940ec254e525a87a6e5753412
Alfred Chin <alfredchin2005@gmail.com>
2023-06-09 09:40:28 +0800

    Add error page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+function Home() {
+  return (
+    <Container>
+      <h1 className="my-3">Your todos</h1>
+    </Container>
+  )
+}
+
+export default Home
