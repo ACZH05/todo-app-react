4928e61f5dcf29a14e7da47b304844c169eed328
Alfred Chin <alfredchin2005@gmail.com>
2023-06-09 10:04:25 +0800

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { Navbar, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ErrorPage from './pages/ErrorPage'
+
+function Layout() {
+  return (
+    <>
+      <Navbar bg='light' variant='light'>
+        <Container>
+          <Navbar.Brand href='/'>Todos</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  )
+}
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path='*' element={<ErrorPage />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
