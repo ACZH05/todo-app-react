de425df7ce4bcfc179514b03bb1f73f2e531532b
Alfred Chin <alfredchin2005@gmail.com>
2023-06-09 11:02:50 +0800

    Add todoContext Provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import ErrorPage from './pages/ErrorPage'
 import AddTodo from './pages/AddTodo';
+import { useLocalStorage } from 'use-local-storage'
+import { TodoContext } from './contexts/TodoContext';
 
 function Layout() {
   return (
@@ -22,16 +24,19 @@ function Layout() {
 }
 
 function App() {
+  const [todos, setTodos] = useLocalStorage("Todos", [])
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path='add' element={<AddTodo />}/>
-          <Route path='*' element={<ErrorPage />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <TodoContext.Provider value={ {todos, setTodos} }>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />}/>
+            <Route path='add' element={<AddTodo />}/>
+            <Route path='*' element={<ErrorPage />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TodoContext.Provider>
   )
 }
 
