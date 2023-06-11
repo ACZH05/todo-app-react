427e050e603944d5fe55ccaf795c43a9aff54419
Alfred Chin <alfredchin2005@gmail.com>
2023-06-11 13:38:39 +0800

    Add delete function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,35 +4,40 @@ import { Badge, Card, Col, Row } from 'react-bootstrap'
 import { TodoContext } from "../contexts/TodoContext"
 import { BsTrash3Fill } from 'react-icons/bs'
 
-function CardGroup({ todos }) {
-    return todos.map((todo) => {
-        const completed = todo.completed
-        const bg = completed ? "success" : "danger"
+function CardGroup({ todos, deleteCard }) {  
+  return todos.map((todo) => {
+      const completed = todo.completed
+      const bg = completed ? "success" : "danger"
 
-        return (
-            <Col md={4} key={ todo.id }>
-                <Card className="my-3">
-                    <Card.Body>
-                        <Card.Title>{todo.title}</Card.Title>
-                        <Card.Text>{ todo.description }</Card.Text>
-                        <span className="d-flex justify-content-between">
-                          <Badge bg={bg} className="p-2">{!completed && "Not"} Completed</Badge>
-                          <BsTrash3Fill id={ todo.id } className="text-danger fs-4" role="button" onClick={(e) => console.log(e.currentTarget.id)} />
-                        </span>
-                    </Card.Body>
-                </Card>
-            </Col>
-        )
-    })
+      return (
+          <Col md={4} key={ todo.id }>
+              <Card className="my-3">
+                  <Card.Body>
+                      <Card.Title>{todo.title}</Card.Title>
+                      <Card.Text>{ todo.description }</Card.Text>
+                      <span className="d-flex justify-content-between">
+                        <Badge bg={bg} className="p-2">{!completed && "Not"} Completed</Badge>
+                        <BsTrash3Fill id={ todo.id } className="text-danger fs-4" role="button" onClick={deleteCard} />
+                      </span>
+                  </Card.Body>
+              </Card>
+          </Col>
+      )
+  })
 }
 
 function Home() {
     const todos = useContext(TodoContext).todos
+    const setTodos = useContext(TodoContext).setTodos
+    function deleteCard(e) {
+      const cardId = parseInt(e.currentTarget.id)
+      setTodos(todos.filter(element => element.id !== cardId))
+    }
   return (
     <Container>
       <h1 className="my-3">Your todos</h1>
       <Row>
-        <CardGroup todos={todos} />
+        <CardGroup todos={todos} deleteCard={deleteCard} />
       </Row>
     </Container>
   )
